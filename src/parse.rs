0833b6b4b3dd79925263e259ced6b5a3a4f0189d
Luke Sneeringer <luke@sneeringer.com>
2024-03-14 12:57:30 -0700

    feat: Support serde, `std::str::FromStr`.

---
+++ src/parse.rs
@@ -0,0 +1,67 @@
+use std::fmt;
+use std::str::FromStr;
+
+use crate::Date;
+
+impl FromStr for Date {
+  type Err = ParseError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    macro_rules! fail {
+      ($s:ident, $r:literal) => {
+        ParseError { src: $s.into(), reason: Some($r) }
+      };
+    }
+    macro_rules! assert {
+      ($s:ident, $e:expr, $r:literal) => {
+        if !($e) {
+          Err(fail!($s, $r))?;
+        }
+      };
+    }
+    let pieces: Vec<&str> = s.split('-').collect();
+    assert!(s, pieces.len() == 3, "Too many components in date.");
+    assert!(s, pieces[0].len() == 4, "Invalid year length.");
+    assert!(s, pieces[1].len() == 2, "Invalid month length.");
+    assert!(s, pieces[2].len() == 2, "Invalid day length.");
+    let year = pieces[0].parse::<i16>().map_err(|_| fail!(s, "Failed to parse year"))?;
+    let month = pieces[1].parse::<u8>().map_err(|_| fail!(s, "Failed to parse month"))?;
+    let day = pieces[2].parse::<u8>().map_err(|_| fail!(s, "Failed to parse day"))?;
+    Ok(Date::new(year, month, day))
+  }
+}
+
+#[derive(Debug)]
+pub struct ParseError {
+  src: String,
+  reason: Option<&'static str>,
+}
+
+impl std::error::Error for ParseError {}
+
+impl fmt::Display for ParseError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(
+      f,
+      "Parse error attempting to parse Date from {}{}",
+      self.src,
+      self.reason.map(|r| format!(": {}", r)).unwrap_or_default(),
+    )
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use assert2::check;
+
+  use super::*;
+
+  #[test]
+  fn test_parse() -> Result<(), ParseError> {
+    check!("2012-04-21".parse::<Date>()? == date! { 2012-04-21 });
+    check!("2012-4-21".parse::<Date>().is_err());
+    check!("04/21/2012".parse::<Date>().is_err());
+    check!("12-04-21".parse::<Date>().is_err());
+    Ok(())
+  }
+}
---
+++ src/serde.rs
@@ -0,0 +1,57 @@
+use std::fmt;
+
+use serde::de::Visitor;
+use serde::Deserialize;
+use serde::Deserializer;
+use serde::Serialize;
+use serde::Serializer;
+
+use crate::Date;
+
+impl Serialize for Date {
+  fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+    serializer.collect_str(&self.format("%Y-%m-%d"))
+  }
+}
+
+struct DateVisitor;
+
+impl<'de> Visitor<'de> for DateVisitor {
+  type Value = Date;
+
+  fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    formatter.write_str("a YYYY-MM-DD date string")
+  }
+
+  fn visit_str<E: serde::de::Error>(self, s: &str) -> Result<Self::Value, E> {
+    s.parse().map_err(E::custom)
+  }
+}
+
+impl<'de> Deserialize<'de> for Date {
+  fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+    deserializer.deserialize_str(DateVisitor)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use assert2::check;
+
+  use super::*;
+
+  #[test]
+  fn test_serde() -> Result<(), serde_json::Error> {
+    let json = r#"{"date":"2012-04-21"}"#;
+    let struct_: TestStruct = serde_json::from_str(json)?;
+    check!(struct_.date == date! { 2012-04-21 });
+    let json = serde_json::to_string(&struct_)?;
+    check!(json == r#"{"date":"2012-04-21"}"#);
+    Ok(())
+  }
+
+  #[derive(Deserialize, Serialize)]
+  struct TestStruct {
+    date: Date,
+  }
+}
