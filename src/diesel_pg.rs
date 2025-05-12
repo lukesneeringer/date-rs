2e84aede0882c6c54ef27fc732e4379425a9d295
Luke Sneeringer <luke@sneeringer.com>
2025-05-12 15:16:15 -0700

    fix

---
+++ src/diesel_pg.rs
@@ -0,0 +1,31 @@
+//! Serialization to/from PostgreSQL
+
+use diesel::deserialize::FromSql;
+use diesel::deserialize::Result as DeserializeResult;
+use diesel::pg::Pg;
+use diesel::pg::PgValue;
+use diesel::pg::data_types::PgDate;
+use diesel::serialize::Output;
+use diesel::serialize::Result as SerializeResult;
+use diesel::serialize::ToSql;
+use diesel::sql_types;
+
+use crate::Date;
+use crate::interval::DateInterval;
+
+impl ToSql<sql_types::Date, Pg> for Date {
+  fn to_sql<'se>(&'se self, out: &mut Output<'se, '_, Pg>) -> SerializeResult {
+    let days_since_epoch = (*self - PG_EPOCH).days();
+    ToSql::<sql_types::Date, Pg>::to_sql(&PgDate(days_since_epoch), &mut out.reborrow())
+  }
+}
+
+impl FromSql<sql_types::Date, Pg> for Date {
+  fn from_sql(bytes: PgValue<'_>) -> DeserializeResult<Self> {
+    let PgDate(offset) = FromSql::<diesel::sql_types::Date, Pg>::from_sql(bytes)?;
+    let duration = DateInterval::new(offset);
+    Ok(PG_EPOCH + duration)
+  }
+}
+
+const PG_EPOCH: Date = date! { 2000-01-01 };
---
+++ src/duckdb.rs
@@ -0,0 +1,45 @@
+//! Integration with DuckDB.
+
+use duckdb::Result;
+use duckdb::types::FromSql;
+use duckdb::types::FromSqlError;
+use duckdb::types::FromSqlResult;
+use duckdb::types::ToSql;
+use duckdb::types::ToSqlOutput;
+use duckdb::types::ValueRef;
+
+use crate::Date;
+
+impl FromSql for Date {
+  fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+    match value {
+      ValueRef::Date32(d) => Ok(Self(d)),
+      _ => Err(FromSqlError::InvalidType),
+    }
+  }
+}
+
+impl ToSql for Date {
+  fn to_sql(&self) -> Result<ToSqlOutput<'_>> {
+    Ok(ToSqlOutput::Borrowed(ValueRef::Date32(self.0)))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use assert2::check;
+
+  use super::*;
+
+  #[test]
+  fn test_to_sql() -> Result<()> {
+    let dt = date! { 2012-04-21 };
+    let output = dt.to_sql()?;
+    if let ToSqlOutput::Borrowed(ValueRef::Date32(i)) = output {
+      check!(i == 20_000);
+    } else {
+      check!(false, "Incorrect type");
+    }
+    Ok(())
+  }
+}
